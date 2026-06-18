55f9d070f1b5b876b4e32254be3b894a7d2e643a
ujjwalchauhan279 <ujjwalchauhan279@gmail.com>
2026-06-18 21:48:06 +0530

    day4

---
+++ Week3/Day4/Daily_Temperatures.java
@@ -0,0 +1,19 @@
+package Week3.Day4;
+
+import java.util.Stack;
+
+public class Daily_Temperatures {
+    public int[] dailyTemperatures(int[] temps) {
+        int[] results = new int[temps.length];
+        Stack<Integer> stack = new Stack<>();
+        /// UPVOTE !
+        for (int i = 0; i < temps.length; i++) {
+            while (!stack.isEmpty() && temps[stack.peek()] < temps[i]) {
+                results[stack.peek()] = i - stack.pop();
+            }
+            stack.push(i);
+        }
+
+        return results;
+    }
+}
