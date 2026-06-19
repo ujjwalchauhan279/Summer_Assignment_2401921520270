65dee1377ce83718ec6c2b033c6ee2affbe0b2bf
ujjwalchauhan279 <ujjwalchauhan279@gmail.com>
2026-06-19 21:44:13 +0530

    Day5

---
+++ Week3/Day5/Implement_Queue_using_Stacks.java
@@ -0,0 +1,29 @@
+package Week3.Day5;
+
+import java.util.Stack;
+
+class MyQueue {
+
+    Stack<Integer> input = new Stack();
+    Stack<Integer> output = new Stack();
+
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public void pop() {
+        peek();
+        output.pop();
+    }
+
+    public int peek() {
+        if (output.empty())
+            while (!input.empty())
+                output.push(input.pop());
+        return output.peek();
+    }
+
+    public boolean empty() {
+        return input.empty() && output.empty();
+    }
+}
